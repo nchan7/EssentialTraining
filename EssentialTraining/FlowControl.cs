623f8b4fb90daee901bb6c63b44d1930a1f5c9cc
Nathan Chan <NChan@nbbj.com>
2020-05-18 17:12:15 -0700

    added flow control and programming loops classes

---
+++ EssentialTraining/FlowControl.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class FlowControl
+    {
+        public string IsYourNameNotBruce(string yourName)
+        {
+            if(yourName.ToLower() != "bruce")
+            {
+                return "Can we call you Bruce for the sake of simplicity?";
+            } else
+            {
+                return "G'day Bruce";
+            }
+        }
+        public string GradeLetter(int score)
+        {
+            if (score > 90)
+            {
+                return "A";
+            }
+            else if (score > 79 && score < 90)
+            {
+                return "B";
+            } else if (score >= 70 && score <= 80)
+            {
+                return "C";
+            } else
+            {
+                return "F";
+            }
+        }
+        public string PrimaryOrSecondaryCompound(string color)
+        {
+            if(color.ToLower() == "red" || color.ToLower() == "blue" || color.ToLower() == "yellow")
+            {
+                return "Primary";
+            } else
+            {
+                return "Secondary";
+            }
+        }
+        public string SecondaryOrPrimary(string color)
+        {
+            var result = "";
+            switch(color.ToLower())
+            {
+                case "red":
+                    result = "Primary";
+                    // could have more statements
+                    break;
+
+                case "blue":
+                    result = "Primary";
+                    break;
+
+                case "yellow":
+                    result = "Primary";
+                    break;
+
+                default:
+                    result = "Secondary";
+                    break;
+            }
+            return result;
+        }
+        public string PrimaryOrSecondary(string color)
+        {
+            var result = "";
+            if (color.ToLower() == "red")
+            {
+                result = "Primary";
+            }
+            else if (color.ToLower() == "blue")
+            {
+                result = "Primary";
+            }
+            else if (color.ToLower() == "yellow")
+            {
+                result = "Primary";
+            } else
+            {
+                result = "Secondary";
+            }
+            return result;
+        }
+        public bool IsYourFavoriteColorYellow(string color)
+        {
+            return (color.ToLower() == "yellow");
+        }
+        public bool IsYourFavoriteColorGreen(string color)
+        {
+            return (color.ToLower() == "Green") ? true : false;
+        }
+        public bool IsYourFavoriteColorRed(string color)
+        {
+            if (color.ToLower() == "red") return true;
+            return false;
+        }
+        public bool IsYourFavoriteColorBlue(string color)
+        {
+            if( color.ToLower() == "blue")
+            {
+                return true; 
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EssentialTraining/ProgrammingLoops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class ProgrammingLoops
+    {
+        public int ForLoop()
+        {
+            var sum = 0;
+            for(var i = 0; i < 100; i++)
+            {
+                sum = sum + i;
+            }
+            return sum; 
+        }
+    }
+}
