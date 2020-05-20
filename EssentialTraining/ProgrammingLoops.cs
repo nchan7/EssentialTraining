f3bb317e4641f6204ae3726ed907f165b5abe86c
Nathan Chan <NChan@nbbj.com>
2020-05-20 16:31:43 -0700

    completed course which includes exception handling, production, NuGet and NLog

--- EssentialTraining/ProgrammingLoops.cs
+++ EssentialTraining/ProgrammingLoops.cs
@@ -3,9 +3,57 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace EssentialTraining
-{
+{    
     public class ProgrammingLoops
     {
+        public string BreakAndContinue()
+        {
+            var sb = new StringBuilder();
+            var words = new List<string>() { "Bread", "Milk", "Eggs", "Cheese", "Apples" };
+            foreach (var word in words)
+            {
+                if (word.StartsWith("M")) continue;
+
+                if (word.StartsWith("C")) break;
+
+                sb.AppendLine(word);
+            }
+            return sb.ToString();
+        }
+        public int DoWhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            do
+            {
+                sum += counter;
+                counter++;
+            } while (counter < 100);
+
+            return sum; 
+        }
+        public int WhileLoop()
+        {
+            var sum = 0;
+            var counter = 0;
+            while(counter < 100)
+            {
+                sum += counter;
+                counter++;
+            }
+            return sum;
+        }
+        public int ForEachLoop()
+        {
+            var numbers = new List<int>() { 1, 3, 5, 7, 9 };
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
         public int ForLoop()
         {
             var sum = 0;
---
+++ EssentialTrainingApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NLog;
+
+namespace EssentialTrainingApp
+{
+    class Program
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+        public static List<string> Words;
+        static void Main(string[] args)
+        {
+            logger.Trace("The program started.");
+            Words = new List<string>();
+            Words.Add("Bread");
+            Words.Add("Milk");
+            Words.Add("Cheese");
+
+            CrazyMathProblem();
+            ReadTextFile();
+            Console.ReadLine();
+        }
+
+        private static void ReadTextFile()
+        {
+            try
+             {
+                using (var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            } 
+            catch(System.IO.DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Couldn't find the directory.");
+                logger.Error("The directory was not found." + ex.Message);
+            }
+            catch(System.IO.FileNotFoundException ex)
+            {
+                Console.WriteLine("Couldn't find the file.");
+                logger.Error(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("An unknown error occured " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The finally runs all the time");
+            }
+}
+
+        private static int CrazyMathProblem()
+        {
+            var income = 1000;
+            for (var i = 10; i > 0; i--)
+            {
+                income = income = (income / i);
+            }
+
+            return income;
+        }
+    }
+}
